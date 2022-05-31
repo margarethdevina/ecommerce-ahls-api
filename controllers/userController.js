c8b82c96cb30ec30ead4f9b973a106a02e18a736
margarethdevina <63775806+margarethdevina@users.noreply.github.com>
2022-05-31 20:38:45 +0700

    login query diupdate

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,11 @@ module.exports = {
         try {
             let resultsUsers = await dbQuery('Select id, username, email, role FROM users;')
 
-            // let resultsCart = await dbQuery('Select * FROM cart;')
-
-            let resJoinCartStocksProductsImage = await dbQuery(`select p.nama, i.image, p.harga, s.type, s.qty as stockQty, c.* from cart c JOIN stocks s on c.idstock = s.idstock JOIN products p on s.idproducts = p.id JOIN image i on p.id = i.idProduct group by c.idcart;`)
+            let resultsCart = await dbQuery(`select p.nama, i.image, p.harga, s.type, s.qty as stockQty, c.* from cart c 
+            JOIN stocks s on c.idstock = s.idstock 
+            JOIN products p on s.idproducts = p.id 
+            JOIN image i on p.id = i.idProduct 
+            group by c.idcart;`)
 
             // let resJoinCartStocksProductsImage = await dbQuery('select c.idcart, c.iduser, c.idstock, c.qty, c.subTotal, s.idstock, s.idproducts, s.type, s.qty as stockQty, p.nama, p.harga, i.image from cart c JOIN stocks s on c.idstock = s.idstock JOIN products p JOIN image i on s.idproducts = p.id = i.idProduct;')
 
@@ -15,7 +17,7 @@ module.exports = {
 
             resultsUsers.forEach((val, idx) => {
                 val.cart = [];
-                resJoinCartStocksProductsImage.forEach((valCart, idxCart) => {
+                resultsCart.forEach((valCart, idxCart) => {
                     if (val.id == valCart.iduser) {
                         val.cart.push(valCart)
                     }
@@ -59,37 +61,59 @@ module.exports = {
         res.status(200).send("<h2>REGISTER</h2>")
 
     },
-    login: (req, res, next) => {
+    login: async (req, res, next) => {
+        try {
+            // console.log(req.body)
+            let resultsLogin = await dbQuery(`Select id, username, email, role FROM users where email="${req.body.email}" and password="${req.body.password}";`)
 
-        dbConf.query(`Select id, username, email, role FROM users where email="${req.body.email}" and password="${req.body.password}";`, (error, resultsUser) => {
-            console.log(req.body)
-            if (error) {
-                console.log(error);
-                return next(error);
+            if(resultsLogin.length == 1){
+                let resultsCart = await dbQuery(`select p.nama, i.image, p.harga, s.type, s.qty as stockQty, c.* from cart c 
+                JOIN stocks s on c.idstock = s.idstock 
+                JOIN products p on s.idproducts = p.id 
+                JOIN image i on p.id = i.idProduct 
+                where c.iduser = ${resultsLogin[0].id} 
+                group by c.idcart ;`)
+
+                resultsLogin[0].cart = resultsCart;
+                //karena login dan pasti cuma 1 data yg direturn jangan lupa tambah [0] setelah resultsLogin
+                return res.status(200).send(resultsLogin[0]);
+            } else {
+                return res.status(404).send({
+                    success:false,
+                    message:"user not found"
+                });
             }
 
-            dbConf.query('Select * FROM cart;', (errorCart, resultsCart) => {
-                if (errorCart) {
-                    return next(errorCart);
-                }
+        } catch (error) {
+            return next(error);
+        }
 
-                console.log(resultsUser);
-                console.log(resultsCart);
+        // dbConf.query(`Select id, username, email, role FROM users where email="${req.body.email}" and password="${req.body.password}";`, (error, resultsUser) => {
+        //     console.log(req.body)
+        //     if (error) {
+        //         console.log(error);
+        //         return next(error);
+        //     }
 
-                resultsUser.forEach((val, idx) => {
-                    val.cart = [];
-                    resultsCart.forEach((valCart, idxCart) => {
-                        if (val.id == valCart.iduser) {
-                            val.cart.push(valCart)
-                        }
-                    })
-                })
+        //     dbConf.query('Select * FROM cart;', (errorCart, resultsCart) => {
+        //         if (errorCart) {
+        //             return next(errorCart);
+        //         }
 
-                return res.status(200).send(resultsUser);
+        //         resultsUser.forEach((val, idx) => {
+        //             val.cart = [];
+        //             resultsCart.forEach((valCart, idxCart) => {
+        //                 if (val.id == valCart.iduser) {
+        //                     val.cart.push(valCart)
+        //                 }
+        //             })
+        //         })
 
-            })
+        //         return res.status(200).send(resultsUser);
 
-        })
+        //     })
+
+        // })
     },
     edit: (req, res, next) => {
 
